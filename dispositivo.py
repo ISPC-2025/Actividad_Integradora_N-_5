5f7b3b024c7f3409f87cc28178afb46a5900602a
Lazarte Mansicidor Alexis Saul <Alexis_lazarte@hotmail.com>
2025-09-12 20:00:40 -0300

    Se realizo cambien para interactuar con los metodos

--- dispositivo.py
+++ dispositivo.py
@@ -8,15 +8,15 @@ class Dispositivo:
         self.esencial = esencial
 
     def encender(self):
-        return "El dispositivo esta encendido."
+        return f"\nId: {self.id} - Nombre: {self.nombre} - Tipo: {self.tipo_id} - Estado: { '1' if self.estado_id == True else '0'} - Ubicacion: {self.ubicacion} - Esencial: {'Prendido' if self.esencial == True else 'Apagado'}"
     
     def apagar(self):
-        return "El dispositivo se apagó."
+        return f"\nId: {self.id} - Nombre: {self.nombre} - Tipo: {self.tipo_id} - Estado: {'0' if self.estado_id == True else '1'} - Ubicacion: {self.ubicacion} - Esencial: {'Apagado' if self.esencial == True else 'Prendido'}"
     
     def ajustar_configuracion(self):
-        return "Su configuracion se guardo correctamente."
+        return "\nSu configuracion se guardo correctamente."
     
-mi_dispositivo = Dispositivo(1, "Microondas", "Electrodomestico", "Apagado", "Cocina", True)
+mi_dispositivo = Dispositivo(1, "Microondas", 1, 1, "Cocina", True)
 
 print(mi_dispositivo.encender())
 print(mi_dispositivo.apagar())
